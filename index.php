61a631754414263766f349dfe7a28d39e63a1c65
Mudegu Brian <49227624+TycoonMudegu@users.noreply.github.com>
2024-05-06 15:49:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,25 +6,20 @@ $uri = $_SERVER['REQUEST_URI'];
 // Extract the path before the query string
 $path = strstr($uri, '?', true) ?: $uri;
 
-// Define the base directory
-$baseDir = 'https://tycoontest-92caab624ce2.herokuapp.com';
-
 // Map URIs to corresponding PHP files
 $routes = [
-    $baseDir . '/welcome' => 'home.php',
-    $baseDir . '/about' => 'about.php',
-    $baseDir . '/potfolio' => 'potfolio.php',
-    $baseDir . '/Reachout' => 'reachout.php',
-
-    $baseDir . '/flexing' => 'letsflex.php',
-   
+    '/welcome' => 'home.php',
+    '/about' => 'about.php',
+    '/portfolio' => 'portfolio.php',
+    '/reachout' => 'reachout.php',
+    '/flexing' => 'letsflex.php',
     // Add more routes as needed
 ];
 
 // Check if the current URI is in the routes array
 if (array_key_exists($path, $routes)) {
     require_once $routes[$path];
-} elseif ($path == $baseDir . '/home' || $path == $baseDir . '/') {
+} elseif ($path == '/' || $path == '/home') {
     require_once 'home.php';
 } else {
     // If no route matches, include the 404 page
